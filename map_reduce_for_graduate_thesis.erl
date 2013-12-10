ba78d1bf72e24816f2efaf88b05349dfff68c9ed
zz chen <grantchen88@gmail.com>
2013-12-10 19:36:12 +0800

    zz'

--- map_reduce_for_graduate_thesis.erl
+++ map_reduce_for_graduate_thesis.erl
@@ -1,7 +1,6 @@
 -module(mymap).
 -import(lists,[foreach/2,map/2]).
 -export([map/2]).
-
 % call map() function with parameter F = process_doc_word
 map(F,L) -> 
 	S = self(),
@@ -15,7 +14,6 @@ map(F,L) ->
 % send the result back after processing done
 do_f(Parent,Ref,F,I) ->
 	Parent ! {Ref,(catch F(I))}.
-
 % do the reducing, gather the result 
 gather(0,_,Res) -> Res;
 gather(N,Ref,Res) ->
@@ -25,9 +23,8 @@ gather(N,Ref,Res) ->
 
 % process the {doc,[word1,word2,word3..]}
 % return a list, e.g.[{word1,doc},{word2,doc}, {word3,doc}..]
-process_doc_word({docid,[]},res) -> res;	% processing done
-process_doc_word({docid,[next_word|to_be_processing],res) ->	% still need to process
-	process_doc_word({docid,to_be_processing},[{next_word,docid}|res]).
-
+process_doc_word({Docid,[]},Res) -> Res;	% processing done
+process_doc_word({Docid,[Next_word|To_be_processing],Res) ->	% still need to process
+	process_doc_word({Docid,To_be_processing},[{Next_word,Docid}|Res]).
 
 
