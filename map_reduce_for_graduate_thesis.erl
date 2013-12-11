d3f67b1e5d3708fb89ed53d0bffeafdc8baf4494
zz chen <grantchen88@gmail.com>
2013-12-11 11:38:39 +0800

    map reduce

--- map_reduce_for_graduate_thesis.erl
+++ map_reduce_for_graduate_thesis.erl
@@ -8,23 +8,23 @@ map(F,L) ->
 	foreach(fun(I) -> 
 			spawn(fun() -> do_f(S,Ref,F,I) end)
 			end,L),
-	gather(length(L),Ref,[]).
+	reduce(length(L),Ref,[]).
 
 % process for each ( doc : <word1,word2,word3..> ) in F
 % send the result back after processing done
 do_f(Parent,Ref,F,I) ->
-	Parent ! {Ref,(catch F(I))}.
-% do the reducing, gather the result 
-gather(0,_,Res) -> Res;
-gather(N,Ref,Res) ->
-	receive
-	{Ref,Ret} -> gather(N-1,Ref,[Ret|Res])
-	end.
+	Parent ! {Ref,(catch F(I,[]))}.
 
 % process the {doc,[word1,word2,word3..]}
 % return a list, e.g.[{word1,doc},{word2,doc}, {word3,doc}..]
 process_doc_word({Docid,[]},Res) -> Res;	% processing done
-process_doc_word({Docid,[Next_word|To_be_processing],Res) ->	% still need to process
+process_doc_word({Docid,[Next_word|To_be_processing]},Res) ->	% still need to process
 	process_doc_word({Docid,To_be_processing},[{Next_word,Docid}|Res]).
 
+% do the reducing, gather the result 
+reduce(0,_,Res) -> Res;
+reduce(N,Ref,Res) ->
+	receive
+	{Ref,Ret} -> reduce(N-1,Ref,[Ret|Res])
+	end.
 
